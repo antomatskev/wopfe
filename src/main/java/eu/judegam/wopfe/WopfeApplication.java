48066d0436340254201aaabc7dbc0f916a39ffd5
Anton-7 <antmat@ttu.ee>
2021-02-08 10:21:27 +0200

    initial commit

---
+++ src/main/java/eu/judegam/wopfe/WopfeApplication.java
@@ -0,0 +1,13 @@
+package eu.judegam.wopfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WopfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WopfeApplication.class, args);
+    }
+
+}

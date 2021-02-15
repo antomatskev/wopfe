231de7ad422054c714743a41e8254fcb317ea9cd
Anton-7 <antmat@ttu.ee>
2021-02-15 07:55:23 +0200

    issue26: person class and status enum

---
+++ src/main/java/eu/judegam/wopfe/models/user/Person.java
@@ -0,0 +1,59 @@
+package eu.judegam.wopfe.models.user;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String lastName;
+    private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, String lastName, Status status) {
+        this.name = name;
+        this.lastName = lastName;
+        this.status = status;
+    }
+}

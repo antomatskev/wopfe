f178a135b96bde4d58c1f73ec6248f6350bf1dd8
Juliadjomina <judjom@ttu.ee>
2021-02-21 10:59:20 +0300

    The School class is fixed

--- src/main/java/eu/judegam/wopfe/models/user/School.java
+++ src/main/java/eu/judegam/wopfe/models/user/School.java
@@ -2,65 +2,48 @@ package eu.judegam.wopfe.models.user;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Represents a school.
  */
 @Entity
 public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    public Long getId() {
-        return id;
-    }
-//    private int id;
-//    private String name;
-//    private String location;
+    private String name;
+    private String location;
 //    private List<Teacher> teachers;
 //    private List<Class> classes;
 //    private List<Principals> principals;
-//    private String places;
-//
-//    public School(int id, String name, String location, List<Teacher> teachers, List<Class> classes, List<Principals> principals, String places) {
-//        this.id = id;
-//        this.name = name;
-//        this.location = location;
-//        this.teachers = teachers;
-//        this.classes = classes;
-//        this.principals = principals;
-//        this.places = places;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getLocation() {
-//        return location;
-//    }
-//
-//    public void setLocation(String location) {
-//        this.location = location;
-//    }
-//
+    private String places;
+
+    public School() {
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 //    public List<Teacher> getTeachers() {
 //        return teachers;
 //    }
@@ -84,14 +67,14 @@ public class School {
 //    public void setPrincipals(List<Principals> principals) {
 //        this.principals = principals;
 //    }
-//
-//    public String getPlaces() {
-//        return places;
-//    }
-//
-//    public void setPlaces(String places) {
-//        this.places = places;
-//    }
+
+    public String getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(String places) {
+        this.places = places;
+    }
 
 
 }

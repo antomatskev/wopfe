3af6a67465b21186798f3376a512a5a1e3b3be74
elgedr <elgedr@ttu.ee>
2021-02-23 19:56:00 +0200

    issue24 : Class class is created

---
+++ src/main/java/eu/judegam/wopfe/models/user/Class.java
@@ -0,0 +1,69 @@
+package eu.judegam.wopfe.models.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Class of a student where he/she is studying(
+ */
+@Entity
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String school;
+
+    private String students;
+    private String timetable;
+
+    public Class() {
+    }
+
+    public Class(String name, String school) {
+        this.name = name;
+        this.school = school;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getStudents() {
+        return students;
+    }
+
+    public void setStudents(String students) {
+        this.students = students;
+    }
+
+    public String getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(String timetable) {
+        this.timetable = timetable;
+    }
+}

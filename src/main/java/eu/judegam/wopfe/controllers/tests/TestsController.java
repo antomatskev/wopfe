6eb7038cbcec236e8dd636ba30b1a43ee89f5cfa
Juliadjomina <judjom@ttu.ee>
2021-03-04 16:00:49 +0300

    4:00

--- src/main/java/eu/judegam/wopfe/controllers/tests/TestsController.java
+++ src/main/java/eu/judegam/wopfe/controllers/tests/TestsController.java
@@ -3,7 +3,6 @@ package eu.judegam.wopfe.controllers.tests;
 import eu.judegam.wopfe.models.tests.Test;
 import eu.judegam.wopfe.tests.service.TestsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;

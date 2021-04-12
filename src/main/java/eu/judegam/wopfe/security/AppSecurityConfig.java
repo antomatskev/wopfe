3d949536d89cc0370f71a7ed94c277bf6ea92f37
Anton-7 <antmat@ttu.ee>
2021-04-12 09:40:49 +0300

    issue31: basic role-based implemented

--- src/main/java/eu/judegam/wopfe/security/AppSecurityConfig.java
+++ src/main/java/eu/judegam/wopfe/security/AppSecurityConfig.java
@@ -1,24 +1,57 @@
 package eu.judegam.wopfe.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AppSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/resources/**", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "/resources/**", "/css/*", "/js/*").permitAll()
+                .antMatchers("/main/timetables/**").hasRole(AppUserRole.STUDENT.name())
                 .anyRequest()
 //                .not().anonymous()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmith = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(AppUserRole.STUDENT.name())
+                .build();
+        UserDetails linda = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(AppUserRole.ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(
+                annaSmith,
+                linda
+        );
     }
 }
---
+++ src/main/java/eu/judegam/wopfe/security/AppUserRole.java
@@ -0,0 +1,21 @@
+package eu.judegam.wopfe.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+public enum AppUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(AppUserPermission.COURSE_READ, AppUserPermission.COURSE_WRITE, AppUserPermission.STUDENT_READ,
+            AppUserPermission.STUDENT_WRITE));
+
+    private final Set<AppUserPermission> permissions;
+
+    AppUserRole(Set<AppUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<AppUserPermission> getPermissions() {
+        return permissions;
+    }
+}

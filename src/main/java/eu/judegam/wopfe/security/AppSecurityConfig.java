8b429bc3c418ce61eed8e553628fd0dd8bd4bdc4
Anton-7 <antmat@ttu.ee>
2021-04-12 10:22:32 +0300

    issue31: added authorities example

--- src/main/java/eu/judegam/wopfe/security/AppSecurityConfig.java
+++ src/main/java/eu/judegam/wopfe/security/AppSecurityConfig.java
@@ -3,6 +3,7 @@ package eu.judegam.wopfe.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -26,11 +27,15 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/resources/**", "/css/*", "/js/*").permitAll()
-                .antMatchers("/main/timetables/**").hasRole(AppUserRole.STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/main/teacher/**").hasAuthority(AppUserPermission.STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/main/teacher/**").hasAuthority(AppUserPermission.STUDENT_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/main/teacher/**").hasAuthority(AppUserPermission.STUDENT_WRITE.getPermission())
+                .antMatchers("/main/teacher/**").hasAnyRole(AppUserRole.ADMIN.name(), AppUserRole.ADMIN_TRAINEE.name())
+                .antMatchers(HttpMethod.GET, "/main/timetables/**").hasRole(AppUserRole.STUDENT.name())
                 .anyRequest()
-//                .not().anonymous()
                 .authenticated()
                 .and()
                 .httpBasic();
@@ -42,16 +47,25 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmith = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(AppUserRole.STUDENT.name())
+//                .roles(AppUserRole.STUDENT.name())
+                .authorities(AppUserRole.STUDENT.getGrantedAuthority())
                 .build();
         UserDetails linda = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(AppUserRole.ADMIN.name())
+//                .roles(AppUserRole.ADMIN.name())
+                .authorities(AppUserRole.ADMIN.getGrantedAuthority())
+                .build();
+        UserDetails tom = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(AppUserRole.ADMIN_TRAINEE.name())
+                .authorities(AppUserRole.ADMIN_TRAINEE.getGrantedAuthority())
                 .build();
         return new InMemoryUserDetailsManager(
                 annaSmith,
-                linda
+                linda,
+                tom
         );
     }
 }
--- src/main/java/eu/judegam/wopfe/security/AppUserRole.java
+++ src/main/java/eu/judegam/wopfe/security/AppUserRole.java
@@ -1,13 +1,17 @@
 package eu.judegam.wopfe.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum AppUserRole {
     STUDENT(Sets.newHashSet()),
     ADMIN(Sets.newHashSet(AppUserPermission.COURSE_READ, AppUserPermission.COURSE_WRITE, AppUserPermission.STUDENT_READ,
-            AppUserPermission.STUDENT_WRITE));
+            AppUserPermission.STUDENT_WRITE)),
+    ADMIN_TRAINEE(Sets.newHashSet(AppUserPermission.COURSE_READ, AppUserPermission.STUDENT_READ));
 
     private final Set<AppUserPermission> permissions;
 
@@ -18,4 +22,13 @@ public enum AppUserRole {
     public Set<AppUserPermission> getPermissions() {
         return permissions;
     }
+
+    public Set<GrantedAuthority> getGrantedAuthority() {
+        Set<GrantedAuthority> ret = getPermissions().stream()
+                .map(p -> new SimpleGrantedAuthority(p.getPermission()))
+                .collect(Collectors.toSet());
+        ret.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return ret;
+    }
+
 }

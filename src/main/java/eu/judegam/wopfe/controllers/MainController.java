1aa8f53ca031df218ee8968a7cbdd1832f9880d7
elgedr <elgedr@ttu.ee>
2021-04-15 15:52:09 +0300

    issue75

--- src/main/java/eu/judegam/wopfe/controllers/MainController.java
+++ src/main/java/eu/judegam/wopfe/controllers/MainController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String home(Model model) {
         return "home";
     }
